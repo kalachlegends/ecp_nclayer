6206b36d33e4aebe12d7e7115d3b380e21d046c3
artem <artem.kalachev.2002@gmail.com>
2022-12-19 17:45:33 +0600

    stage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,187 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="grid">
+    <a-spin
+      :spinning="isLoad"
+      :tip="'Идет загрузка вебсокета'"
+      :indicator="indicator"
+    >
+      <a-alert :message="message"> </a-alert>
+    </a-spin>
+    <a-alert
+      v-if="isError"
+      message="NCALayer не найден!!!!!!!!!!!!"
+      type="error"
+      show-icon
+    >
+      <template #icon><smile-outlined /></template>
+    </a-alert>
+    <!-- <div style="width: 100%">
+      <h6>MODEL:</h6>
+      <json-editor
+        v-model="jsonView"
+        :show-btns="true"
+        :expandedOnStart="true"
+        @json-change="onJsonChange"
+        @json-save="jsonSave"
+        @has-error="errorJsonb"
+      />
+    </div> -->
+
+    <!-- <div class="width-100p">
+      <a-textarea
+        v-model:value="xmlString"
+        placeholder="Basic usage"
+        :rows="4"
+      />
+      <a-button @click="localSave">
+        Сохранить данный json в local storage</a-button
+      >
+      <a-button @click="podpisat"> Подписать</a-button>
+    </div> -->
+    <div>
+      <a-typography-title>Введите XML</a-typography-title>
+      <a-textarea
+        v-model:value="xmlString"
+        placeholder="Введите XML"
+        :rows="4"
+      />
+      <a-button @click="podpisat"> Подписать</a-button>
+      <!-- <json-viewer :value="jsonView" /> -->
+    </div>
+    <div>
+      <a-alert
+        v-if="isErrorNc"
+        :message="messageError"
+        type="error"
+        show-icon
+      />
+
+      <a-typography-title>Подписанный документ:</a-typography-title>
+
+      <!-- <json-viewer :value="nclayerMess" /> -->
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import JsonViewer from "vue-json-viewer";
+import { Vue3JsonEditor } from "vue3-json-editor";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  data() {
+    return {
+      xmlString: "",
+      isError: false,
+      isLoad: true,
+      messageError: "Ошибка подписи",
+      message: "NCALayer загружен....",
+      nclayerMess: "",
+      isErrorNc: false,
+      socket: {},
+      jsonView: {},
+    };
+  },
+  methods: {
+    onJsonChange(e) {
+      this.jsonView = { ...e };
+      console.log(e);
+    },
+    jsonSave(e) {
+      this.jsonView = { ...e };
+      console.log(e);
+    },
+    errorJsonb(e) {
+      console.log(e);
+    },
+    localSave() {
+      localStorage.removeItem("json");
+      localStorage.setItem("json", JSON.stringify(this.jsonView));
+    },
+    podpisat() {
+      this.socket.send(
+        JSON.stringify({
+          module: "kz.gov.pki.knca.commonUtils",
+          method: "signXml",
+          args: ["PKCS12", "SIGNATURE", this.xmlString, "", ""],
+        })
+      );
+    },
+    components: {
+      JsonViewer,
 
+      Vue3JsonEditor,
+    },
+    computed: {
+      compXmlString() {
+        return this.xmlString;
+      },
+    },
+  },
+  mounted() {
+    this.socket = new WebSocket("wss://127.0.0.1:13579/");
+
+    if (localStorage.getItem("json")) {
+    } else {
+      localStorage.setItem(
+        "json",
+        JSON.stringify({
+          module: "kz.gov.pki.knca.commonUtils",
+          method: "signXml",
+          args: [
+            "PKCS12",
+            "SIGNATURE",
+            '<?xml version="1.0" encoding="UTF-8"?><doc><contents><![CDATA[<div class="text-right">\n  <p><em>Приложение к Приказу АО «Транстелеком»</em></p>\n  <p><em>от 18.06.2019 года № TTC/39-ВП </em></p>\n</div>\n<p></p>\n\n<p></p>\n\n]]></contents></doc>',
+            "",
+            "",
+          ],
+        })
+      );
+    }
+    this.jsonView = JSON.parse(localStorage.getItem("json"));
+    this.socket.onerror = (e) => {
+      this.isError = true;
+    };
+    this.socket.onopen = (e) => {
+      this.isLoad = false;
+    };
+    this.socket.onmessage = (event) => {
+      // this.nclayerMess = JSON.parse(event.data);
+      let data = JSON.parse(event.data);
+
+      if (data.status == 200) {
+        this.isErrorNc = false;
+      }
+    };
+  },
+};
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  width: 1000px;
+}
+.flex {
+  display: flex;
+  gap: 10px;
+}
+.grid div {
+  width: 100%;
+}
+.grid {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+}
+.width-100p {
+  width: 100%;
+}
+.d-flexx {
+  display: flex;
+  gap: 20px;
+  width: 100%;
+  justify-content: space-between;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+import JsonViewer from 'vue-json-viewer'
+import { Vue3JsonEditor } from 'vue3-json-editor'
+app.component("json-editor", Vue3JsonEditor)
+app.use(Antd).use(JsonViewer).mount('#app');
